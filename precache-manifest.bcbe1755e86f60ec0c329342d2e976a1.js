67425de4e2bc5a06467f994b08abdebcd1b58e55
Jake Scudder <scudsterr@gmail.com>
2020-08-20 11:40:19 -0400

    Updates

---
+++ precache-manifest.bcbe1755e86f60ec0c329342d2e976a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e9d486695f5f892604691db10743ba",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "d1f1fdba2dbf5cb002c8",
+    "url": "/hacker-news/static/css/main.ba17825b.chunk.css"
+  },
+  {
+    "revision": "1b7e2d0a074de1dab43c",
+    "url": "/hacker-news/static/js/2.f4a14946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news/static/js/2.f4a14946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f1fdba2dbf5cb002c8",
+    "url": "/hacker-news/static/js/main.e0dd7af3.chunk.js"
+  },
+  {
+    "revision": "dba20c960acd973705b3",
+    "url": "/hacker-news/static/js/runtime-main.41ca198d.js"
+  }
+]);
